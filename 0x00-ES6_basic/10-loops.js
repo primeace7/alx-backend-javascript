c4084585eb5b9f9624f7ab19903c4e09a8e518ae
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-03-06 14:55:15 +0000

    Fix task 10 eslint errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 module.exports = function appendToEachArrayValue(array, appendString) {
   for (const item of array) {
-      array.splice(array.indexOf(item), 1, appendString + item);
+    array.splice(array.indexOf(item), 1, appendString + item);
   }
 
   return array;
